c50e05a0dedea1be1cdfeaabd2261e8d0f360941
Nicholas Weiss <njweiss4@gmail.com>
2022-12-01 00:55:44 -0500

    day1

---
+++ Day1/Day1.ts
@@ -0,0 +1,53 @@
+import * as fs from 'fs';
+
+let input: string = fs.readFileSync("input.txt", 'utf-8');
+
+const part1 = (input:string):number => {
+    let maxCalories: number = 0
+
+    input.split("\n\n").forEach(elf => {
+        let totalCalories: number = 0;
+        elf.split("\n").forEach(calories => {
+            totalCalories += parseInt(calories);
+        })
+    
+        if (totalCalories > maxCalories) {
+            maxCalories = totalCalories;
+        }
+
+    });
+
+    return maxCalories;
+}
+
+const part2 = (input:string):number => {
+    let first: number = 0;
+    let second: number = 0;
+    let third: number = 0;
+
+    input.split("\n\n").forEach(elf => {
+        let totalCalories:number = 0;
+        elf.split("\n").forEach(calories => { 
+            totalCalories += parseInt(calories)
+        });
+
+        if (totalCalories > first) {
+            third = second;
+            second = first;
+            first = totalCalories;
+        }
+        else if (totalCalories > second) {
+            third = second;
+            second = totalCalories;
+        }
+        else if (totalCalories > third) {
+            third = totalCalories;
+        }
+
+    });
+
+    return first + second + third;
+}
+
+console.log(part1(input))
+console.log(part2(input))
